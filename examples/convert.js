4839705f13582952423094cbae1bf718d2f5afbe
YungCaedo <62652990+YungCaedo@users.noreply.github.com>
2023-10-15 09:54:02 +0200

    added example for testing

---
+++ examples/convert.js
@@ -0,0 +1,17 @@
+const webpconv = require('../index.js');
+const converter = new webpconv();
+
+const inputFiles = [
+    'images/animated.webp',
+    'images/animated-transparent.webp',
+    'images/static.webp'
+];
+const outputFolder = 'images/output';
+
+inputFiles.forEach(input => {
+    converter.convert(input, outputFolder, { quality: 15, transparent: '0x000000' }).then(output => {
+        console.log(`converted: ${input} => ${output}`);
+    });
+});
+
+// This will convert all animated/static WebP inputs to GIF/PNG format and output the files in examples/images folder.
