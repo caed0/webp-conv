1e208340e6d5f2eca3cf522c36025947c8216261
caed0 <62652990+caed0@users.noreply.github.com>
2025-02-22 13:05:49 +0100

    Updated examples

--- examples/convert.js
+++ examples/convert.js
@@ -1,9 +1,20 @@
 const webpconv = require('../index.js');
 const converter = new webpconv();
 
-const input = './images/test.webp';
-const output = './images/output/test_c.gif';
+const input = [
+    'examples/images/animated.webp',
+    'examples/images/animated-transparent.webp',
+    'examples/images/static.webp'
+];
 
-converter.convert(input, output, { quality: 15, transparent: '0x000000' }).then(output => {
-    console.log(`converted: ${input} => ${output}`);
-});
+const output = [
+    'examples/images/output/animated.gif',
+    'examples/images/output/animated-transparent.gif',
+    'examples/images/output/static.png'
+];
+
+for (const i in input) {
+    converter.convert(input[i], output[i], { quality: 100, transparent: '0x000000' }).then(output => {
+        console.log(`converted: ${input[i]} => ${output}`);
+    });
+}
