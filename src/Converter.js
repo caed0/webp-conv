b360bc050033d7a9b8f7d948076183ae05eb1c47
caed0 <62652990+caed0@users.noreply.github.com>
2025-02-22 18:39:07 +0100

    fixed transparency issue + static webp conversion

--- src/Converter.js
+++ src/Converter.js
@@ -7,6 +7,10 @@ const { loadImage, createCanvas } = require('canvas');
 const os = require("os");
 
 class Converter {
+  #BINARIES = path.join(path.resolve(__dirname, '..'), 'libwebp', 'bin');
+  #ANIM_DUMP = path.join(this.#BINARIES, 'anim_dump');
+  #DWEBP = path.join(this.#BINARIES, 'dwebp');
+
   async convert(input, output, options = {}) {
     if(!input) throw new Error("Input is required");
     if(!output) throw new Error("Output is required");
@@ -21,10 +25,13 @@ class Converter {
     const transparent = options.transparent || '0x000000';
 
     if(output.endsWith('.png')) {
-      fs.copyFileSync(input, output);
+      await execFile(this.#DWEBP, [input, '-o', output], (error) => {
+        if (error) return console.error(`Error executing anim_dump: ${error}`);
+      });
+
       return output;
     }
-    
+
     const img = new Image();
     await img.load(input);
     await img.initLib();
@@ -38,14 +45,12 @@ class Converter {
     encoder.setTransparent(transparent);
     encoder.setQuality(quality);
 
-    const anim_dump = path.join(path.resolve(__dirname, '..'), 'libwebp', 'bin', 'anim_dump');
     const folder = path.join(os.tmpdir(), 'webp-conv', path.basename(input));
     if (fs.existsSync(folder)) fs.rmSync(folder, { recursive: true });
     fs.mkdirSync(folder, { recursive: true });
 
-    await execFile(anim_dump, ['-folder', folder, input], (error, stdout, stderr) => {
+    await execFile(this.#ANIM_DUMP, ['-folder', folder, input], (error) => {
       if (error) return console.error(`Error executing anim_dump: ${error}`);
-      if (stderr) console.error(`stderr: ${stderr}`);
     });
 
     while (fs.readdirSync(folder).length !== Object.keys(rawFrames).length) {
@@ -59,6 +64,17 @@ class Converter {
       const ctx = createCanvas(width, height).getContext('2d');
       const image = await loadImage(framePath);
       ctx.drawImage(image, 0, 0, width, height);
+
+      const imageData = ctx.getImageData(0, 0, width, height);
+      const data = imageData.data;
+
+      for (let i = 0; i < data.length; i += 4) {
+        if (data[i + 3] > 0 && data[i + 3] < 128) data[i + 3] = 0;
+      }
+
+      // Put the modified pixel data back onto the canvas
+      ctx.putImageData(imageData, 0, 0);
+
       encoder.setDelay(rawFrames[i].delay);
       encoder.addFrame(ctx);
     }
