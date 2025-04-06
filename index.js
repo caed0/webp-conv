6eef55bad4563c48c22eaa1a12a92d4c535c23b2
kaikygr <kaikygomesribeiroof@gmail.com>
2025-04-06 12:27:12 -0400

    add documentation to the main module and create an installation check script for cwebp

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const Converter = require('./src/Converter.js');
+/**
+ * Main module that exports the Converter functionality.
+ *
+ * This module imports the Converter implementation from './src/Converter.js'
+ * and exports it as the default module export.
+ *
+ * @module webp-conv
+ * @requires module:./src/Converter
+ */
+const Converter = require("./src/Converter.js");
 module.exports = Converter;
